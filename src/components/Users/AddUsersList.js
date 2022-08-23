c55a35ac6f5f30399bca4896792cae9d58eefab3
Hector Mejia <mejia7hector@gmail.com>
2022-08-23 10:34:51 -0500

    modal

--- src/components/Users/AddUsersList.js
+++ src/components/Users/AddUsersList.js
@@ -5,7 +5,9 @@ import classes from "./UserList.module.css";
 const AddUsersList = (props) => {
     return <Card className={classes.users}>
         <ul>
-            {props.users.map(user => <li>{user.name} ({user.age} years old)
+            {props.users.map(user => <li key={user.id}>
+                {user.name}
+                ({user.age} years old)
                 </li>
             )}
         </ul>
